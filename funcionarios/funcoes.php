5143c08723c927631ea6c650cbf37b69331c7159
vinicius-araujo777 <vinicius.araujo60@aluno.ce.gov.br>
2026-05-15 20:47:26 -0300

    add arquivos no crud de funcionarios, add funcoes(list,add e select) e começando o index

---
+++ funcionarios/funcoes.php
@@ -0,0 +1,25 @@
+<?php 
+require_once '../conexao.php';
+
+function listarFuncionarios($con) {
+    $stmt = $con->prepare("SELECT * FROM funcionarios");
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_OBJ);
+}
+
+function adicionarFuncionario($con,$nome, $cargo, $salario){
+    $stmt = $con->prepare("INSERT INTO funcionarios (nome, cargo, salario) VALUES (:nome, :cargo, :salario)");
+    $stmt->bindValue(":nome",$nome);
+    $stmt->bindValue(":cargo",$cargo);
+    $stmt->bindValue(":salario",$salario);
+    return $stmt->execute();
+}
+
+function obterFuncionario($con,$id){
+    $stmt = $con->prepare("SELECT * FROM funcionarios WHERE id = :id");
+    $stmt->bindValue(":id",$id);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_OBJ);
+}
+
+?>
---
+++ funcionarios/index.php
@@ -0,0 +1,3 @@
+<?php include '../sidebar.php'; ?>
+
+<h1 class="text-2xl font-bold text-gray-900 mb-6">Funcionários</h1>
